bcfccd4a42467f190a808eb0db81f70803f21b64
Mufudzi Christopher <mnhamoinesu@gmail.com>
2023-12-20 00:20:29 +0000

    Upstream changes to settings.py

--- FoxxMart/FoxxMart/settings.py
+++ FoxxMart/FoxxMart/settings.py
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
     'EXODUS',
     'RickJames',
 
-    'django_browser_reload',
     'crispy_forms',
     'widget_tweaks',
     'taggit',
@@ -162,9 +161,3 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media" )
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'django10.foxx@gmail.com'
-EMAIL_HOST_PASSWORD = 'KingFisher1995!'
